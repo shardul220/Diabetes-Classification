5815f11a02fb0a9891b44b71b44862cc54171a0f
shardul220 <shardul220@gmail.com>
2022-08-05 13:59:53 -0400

    updated

--- app.py
+++ app.py
@@ -16,7 +16,20 @@ def predict():
     '''
     int_features = [float(x) for x in request.form.values()]
     final_features = [np.array(int_features)]
-    prediction = model.predict(final_features)
+    
+    Pregnancies = int(request.form['pregnancies'])
+    DiabetesPedigreeFunction = float(request.form['dpf'])
+    Age = int(request.form['age'])
+    Glucose = int(request.form['glucose'])
+    BloodPressure = int(request.form['bloodpressure'])
+    SkinThickness = int(request.form['skinthickness'])
+    Insulin = int(request.form['insulin'])
+    BMI = float(request.form['bmi'])
+    
+    data = np.array([[Pregnancies, Glucose, 
+                      BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]])
+    
+    prediction = model.predict(data)
 
 
 
